93cb0efb3d09714d712cb2a091a02172541127eb
stefoto85 <you@example.com>
2026-03-27 15:22:00 +0200

    app start

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "arg" {
-  name     = "ContactBookRG"
+  name     = "TaskBoardRG"
   location = "germanywestcentral"
 }
 
@@ -24,7 +24,7 @@ resource "random_integer" "ri" {
 }
 
 resource "azurerm_service_plan" "asp" {
-  name                = "ContactBookASP${random_integer.ri.result}"
+  name                = "TaskBoardASP${random_integer.ri.result}"
   resource_group_name = azurerm_resource_group.arg.name
   location            = azurerm_resource_group.arg.location
   os_type             = "Linux"
@@ -32,23 +32,68 @@ resource "azurerm_service_plan" "asp" {
 }
 
 resource "azurerm_linux_web_app" "alwapp" {
-  name                = "ContactBookApp${random_integer.ri.result}"
+  name                = "TaskBoardApp${random_integer.ri.result}"
   resource_group_name = azurerm_resource_group.arg.name
   location            = azurerm_service_plan.asp.location
   service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
     application_stack {
-      node_version = "20-lts"
+      dotnet_version = "8.0"
 
     }
     always_on = false
   }
+
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.database.name};User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${azurerm_mssql_server.sqlserver.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+
+
+
+  }
 }
+
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = "mssqlserver-${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.arg.name
+  location                     = azurerm_resource_group.arg.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "thisIsKat11"
+
+}
+
+resource "azurerm_mssql_database" "database" {
+  name                 = "task-board-db"
+  server_id            = azurerm_mssql_server.sqlserver.id
+  collation            = "SQL_Latin1_General_CP1_CI_AS"
+  license_type         = "LicenseIncluded"
+  max_size_gb          = 2
+  sku_name             = "S0"
+  zone_redundant       = false
+  storage_account_type = "Local"
+
+
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "azurerm_mssql_firewall_rule" "firewall_rule" {
+  name             = "FirewallRule1"
+  server_id        = azurerm_mssql_server.sqlserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
 
 resource "azurerm_app_service_source_control" "assc" {
   app_id                 = azurerm_linux_web_app.alwapp.id
-  repo_url               = "https://github.com/nakov/ContactBook.git"
-  branch                 = "master"
+  repo_url               = "https://github.com/svdimov/TaskBoard-Terraform.git"
+  branch                 = "main"
   use_manual_integration = true
 }
